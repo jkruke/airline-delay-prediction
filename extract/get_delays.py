1b0fd55b4562e39224e8be29b707d14d935fc3fb
Jonas Krukenberg <jonas@jkrukenberg.de>
2023-10-05 10:28:22 +0700

    save delays to CSV

--- extract/get_delays.py
+++ extract/get_delays.py
@@ -1,15 +1,22 @@
-import requests
+from datetime import datetime
+
 import pandas as pd
+import requests
+
 from extract.config import config
 
 
 def main():
-    print(config.airports)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+
     url = (f"https://airlabs.co/api/v9/delays?delay={MIN_DELAY}&dep_iata={config.airports}"
            f"&type=departures&api_key={config.api_key}")
+    print(f"Requesting {url}")
     data = requests.get(url).json()
-    df = pd.to_json(data)
-    print(df)
+    df = pd.json_normalize(data['response'])
+    print(f"Result: {df}")
+    df.to_csv(f"data/delays/delays-{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv")
 
 
 if __name__ == '__main__':
