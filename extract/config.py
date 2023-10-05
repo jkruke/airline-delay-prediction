456250293b798142ebf08112c0cc49fbb654813f
Jonas Krukenberg <jonas@jkrukenberg.de>
2023-10-05 12:14:58 +0700

    remove airports configuration option

--- extract/config.py
+++ extract/config.py
@@ -9,11 +9,9 @@ class Config:
     api_key: str
     min_delay: int
     country: str
-    airports: str
 
 
 dotenv.load_dotenv()
 config = Config(api_key=os.getenv("API_KEY"),
                 min_delay=int(os.getenv("MIN_DELAY", "31")),
-                country=os.getenv("COUNTRY", "VN"),
-                airports=os.getenv("AIRPORTS", ""))
+                country=os.getenv("COUNTRY", "VN"))
