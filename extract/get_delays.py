ba6a92d7450dd07397ef9ebce1cdfb662506809f
Jonas Krukenberg <jonas@jkrukenberg.de>
2023-10-05 12:08:05 +0700

    combine delays with airport information

--- extract/get_delays.py
+++ extract/get_delays.py
@@ -6,17 +6,44 @@ import requests
 from extract.config import config
 
 
+# you can use OFFLINE_MODE during development to save API calls (uses local files instead):
+OFFLINE_MODE = True
+
+
+def get_delays():
+    if OFFLINE_MODE:
+        delays = pd.read_csv("data/delays/delays-sample.csv")
+    else:
+        url = (f"https://airlabs.co/api/v9/delays?delay={MIN_DELAY}"
+               f"&type=departures&api_key={config.api_key}")
+        print(f"Requesting {url}")
+        data = requests.get(url).json()
+        delays = pd.json_normalize(data['response'])
+    return delays
+
+
 def main():
     pd.set_option('display.max_columns', None)
-    pd.set_option('display.max_rows', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_rows', 5)
 
-    url = (f"https://airlabs.co/api/v9/delays?delay={MIN_DELAY}&dep_iata={config.airports}"
-           f"&type=departures&api_key={config.api_key}")
-    print(f"Requesting {url}")
-    data = requests.get(url).json()
-    df = pd.json_normalize(data['response'])
-    print(f"Result: {df}")
-    df.to_csv(f"data/delays/delays-{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv")
+    delays = get_delays()
+    print(f"Total delays worldwide: {len(delays)}")
+    airports = pd.read_json("data/airports.json")
+    airports["arr_iata"] = airports["iata_code"]
+    airports["dep_iata"] = airports["iata_code"]
+    airports["arr_country_code"] = airports["country_code"]
+    airports["dep_country_code"] = airports["country_code"]
+    delays = pd.merge(delays, airports[["arr_country_code", "arr_iata"]], on="arr_iata", how="inner")
+    delays = pd.merge(delays, airports[["dep_country_code", "dep_iata"]], on="dep_iata", how="inner")
+    delays["domestic"] = delays["arr_country_code"] == delays["dep_country_code"]
+    delays["international"] = ~delays["domestic"]
+    print("Delays:")
+    delays.info()
+    print(delays)
+    delays = delays[["flight_number", "airline_iata", "dep_time_utc", "dep_estimated_utc", "arr_time_utc",
+                     "arr_estimated_utc", "dep_country_code", "arr_country_code", "domestic", "international", "delayed"]]
+    delays.to_csv(f"data/delays/delays-{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv")
 
 
 if __name__ == '__main__':
