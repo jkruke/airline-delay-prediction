680a172e21e9fb0cffce7bfd203a66c2b4b33c32
Jonas Krukenberg <jonas@jkrukenberg.de>
2023-10-12 11:59:29 +0700

    remove min_delay from config

--- extract/config.py
+++ extract/config.py
@@ -7,11 +7,9 @@ import dotenv
 @dataclass
 class Config:
     api_key: str
-    min_delay: int
     country: str
 
 
 dotenv.load_dotenv()
 config = Config(api_key=os.getenv("API_KEY"),
-                min_delay=int(os.getenv("MIN_DELAY", "31")),
                 country=os.getenv("COUNTRY", "VN"))
